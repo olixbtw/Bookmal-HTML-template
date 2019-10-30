f7d5614e12790e5e48fd9c6d9c19c8f2d2dc062a
Oleksandr <oleksandr_revchuk@epam.com>
2019-10-30 13:31:25 +0200

    fixed search broken

--- src/js/resultWindow.js
+++ src/js/resultWindow.js
@@ -21,7 +21,11 @@ const generate = (item) => {
   if (item) return `
     <li class="search-result_item">
       <a href="${item.link}" class="search-result_link">
-        <img class="search-image" src="./assets/images/common/${item.img.src}" alt="${item.img.src}" class="search-result_image">
+        <img 
+          class="search-result_image" 
+          src="./assets/images/common/${item.img.src}" 
+          alt="${item.img.src}" 
+        >
         <span class="search-result_text">${item.title}</span>
       </a>
     </li>
